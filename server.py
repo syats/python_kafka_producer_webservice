ad2d36db7ad843ac8b18ac47c385fae75a2f3bba
mireleschavezv <mireleschavezv@semantic-web.com>
2018-03-30 14:00:46 +0200

    Kafka producing

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from kafka import KafkaProducer
 from io import BytesIO
 
 
@@ -10,15 +11,21 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(b'Hello, world!')
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
+        topic = print(self.headers.get('topic'))
+        broker = print(self.headers.get('broker'))
+        content_length = int(self.headers.get('Content-Length'))
         body = self.rfile.read(content_length)
+
+        producer = KafkaProducer(bootstrap_servers=[broker])
+        producer.send(topic, body)
+        producer.flush()
+
         self.send_response(200)
         self.end_headers()
         response = BytesIO()
-        response.write(b'This is POST request. ')
-        response.write(b'Received: ')
-        response.write(body)
+        response.write(b'sent to kafka ')
         self.wfile.write(response.getvalue())
+
 
 
 httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
